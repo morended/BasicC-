5356bb53cc23087d841c82a94d8a8c37a1179712
mounika.rendedla <mounika.rendedla@extendhealth.com>
2018-06-22 10:53:45 -0600

    Factory Pattern

---
+++ CsharpBasics/DesignPatterns/FactoryMethodPattern.cs
@@ -0,0 +1,143 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace CsharpBasics.DesignPatterns
+{
+
+#region Abstract and Concrete Products
+
+    public abstract class Member
+    {
+        public string Name { get; set; }
+        public string phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string signup()
+        {
+            return "registered";
+        }
+
+        public string login()
+        {
+            return "logged in";
+        }
+
+        public string PersonalInfo()
+        {
+            return Name + phone + Address;
+        }
+    }
+
+    public class BasicMember : Member
+    {
+        public int membershipPrice()
+        {
+            return 10;
+        }
+    }
+
+    public class ClubMember : Member
+    {
+        public int membershipPrice()
+        {
+            return 100;
+        }
+
+        public int discount()
+        {
+            return 10;
+        }
+    }
+
+    #region Membership from Chicago City
+
+    public class ChicagoBasicMember : Member
+    {
+        public int membershipPrice()
+        {
+            return 20;
+        }
+
+        public string JoiningOffer()
+        {
+            return "swimming & Zumba included in basic membership";
+        }
+    }
+
+    public class ChicagoClubMember : Member
+    {
+        public int membershipPrice()
+        {
+            return 200;
+        }
+
+        public int discount()
+        {
+            return 20;
+        }
+    }
+
+    #endregion
+
+    #region Membership from California City
+
+    public class SFOBasicMember : Member
+    {
+        public int membershipPrice()
+        {
+            return 100;
+        }
+
+    }
+
+    public class SFOClubMember : Member
+    {
+        public int membershipPrice()
+        {
+            return 300;
+        }
+
+        public int discount()
+        {
+            return 30;
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Creator Classes - Abstract and  Concrete
+
+    public abstract class MemberShipManager{
+        public void JoinGym(string type)
+        {
+            Member m = CreateObject(type);
+            m.signup();
+            m.login();
+
+        }
+
+        public abstract Member CreateObject(string type);
+
+    }
+
+    public class SFOMemberShipManager : MemberShipManager{
+        public override Member CreateObject(string type)
+        {
+            if (type == "basic") return new SFOBasicMember();
+            return new SFOClubMember();
+        }
+    }
+
+    public class ChicagoMemberShipManager : MemberShipManager
+    {
+        public override Member CreateObject(string type)
+        {
+            if (type == "basic") return new ChicagoBasicMember();
+            return new ChicagoClubMember();
+        }
+    }
+
+    #endregion
+}
---
+++ CsharpBasics/DesignPatterns/SimpleFactory.cs
@@ -0,0 +1,67 @@
+﻿namespace CsharpBasics.DesignPatterns
+{
+    /*Simple Factory concept: This is not any pattern.
+     *In this example we are encapsulating the object creation of Payment Type in to a factory method.
+     * This way the client is decoupled from the object creation and seperated from the code that changes.
+     * Adding or deleting a client can be handled in the factory method "CreatePaymentType"
+   */
+    public class SimpleFactory
+    {
+        public static IPaymentType CreatePaymentType(string type)
+        {
+            if (type.Equals("debit"))
+            {
+                return new DebitCard();
+            }
+            if (type.Equals("credit"))
+            {
+                return new CreditCard();
+            }
+            if (type.Equals("paypal"))
+            {
+                return new PayPal();
+            }
+
+            return null;
+        }
+    }
+
+    public class ClientStore
+    {
+        public string pay(string type)
+        {
+           IPaymentType paymentType = SimpleFactory.CreatePaymentType(type);
+            return paymentType.pay();
+        }
+  
+    }
+
+    public interface IPaymentType
+    {
+         string  pay();
+    }
+
+    public class CreditCard : IPaymentType
+    {
+        public string pay()
+        {
+            return "Payment made using creditcard";
+        }
+    }
+
+    public class PayPal : IPaymentType
+    {
+        public string pay()
+        {
+            return "Payment made using PayPal";
+        }
+    }
+
+    public class DebitCard : IPaymentType
+    {
+        public string pay()
+        {
+            return "Payment made using DebitCard";
+        }
+    }
+}
